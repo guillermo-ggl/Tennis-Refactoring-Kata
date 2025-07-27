fa103dd1596b235ed3c0b7e3537f57222a981988
Guille <g.gutierrezlorenzo@gmail.com>
2025-07-27 21:46:40 +0200

    refactor: extraced ScoreNameWithSomePlayerOverThree

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -31,11 +31,7 @@ namespace Tennis
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = ScoreNameWithSomePLayerOverThree(m_score1, m_score2);
             }
             else
             {
@@ -44,6 +40,15 @@ namespace Tennis
                     +GetScoreNameForThreeOrLess(m_score2);
             }
             return score;
+        }
+
+        private string ScoreNameWithSomePLayerOverThree(int player1Score, int player2Score)
+        {
+            var minusResult = player1Score - player2Score;
+            if (minusResult == 1) return "Advantage player1";
+            if (minusResult == -1) return "Advantage player2";
+            if (minusResult >= 2) return "Win for player1";
+            return "Win for player2";
         }
 
         private string GetScoreForEqualScore(int tempScore)
